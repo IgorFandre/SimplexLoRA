dc23b2ec382064422ba2e435e187b415b3b4c8a2
Igor Shalygin <sutk-i@bk.ru>
2025-04-08 01:02:44 +0300

    Fixed bugs with incorrect multiplicator ranks

--- src/optimizers.py
+++ src/optimizers.py
@@ -361,9 +361,40 @@ def proj_0(x: torch.Tensor, K: int):
     x[idx] = 0.0
     return x
 
-def proj_simplex(x: torch.Tensor, temp: int):
+def proj_simplex_softmax(x: torch.Tensor, temp=1.0):
     x_0 = (x - x.max()) / temp
     return torch.exp(x_0) / torch.exp(x_0).sum()
+
+def proj_simplex_weighted_softmax(x: torch.Tensor, temp=1.0):
+    x_0 = (x - x.max()) / temp
+    return (torch.exp(x_0) * x) / (torch.exp(x_0) * x).sum()
+
+def proj_simplex_euclidean(x: torch.Tensor, temp=None, tau=0.0001, max_iter=1000):
+    '''
+    Bisection for projection onto the simplex
+    FROM: http://www.mblondel.org/publications/mblondel-icpr2014.pdf
+    temp is not used!
+    '''
+    null = torch.Tensor([0]).to(x.device)
+    func = lambda k: torch.sum(torch.maximum(x - k, null)) - 1
+    lower = torch.min(x) - 1 / len(x)
+    upper = torch.max(x)
+
+    for _ in range(max_iter):
+        midpoint = (upper + lower) / 2.0
+        value = func(midpoint)
+
+        if abs(value) <= tau:
+            break
+
+        if value <= 0:
+            upper = midpoint
+        else:
+            lower = midpoint
+
+    ans = torch.maximum(x - midpoint, null)
+    return ans / torch.sum(ans)
+
 
 
 class FatAdamW(optim.Optimizer):
@@ -474,7 +505,7 @@ class FatAdamW(optim.Optimizer):
             
             w_vector = torch.tensor(w_vector)
             print(f"before proj {w_vector}")
-            w_vector = group["proj"](w_vector, self.temp) * len(self.chosen_layers)
+            w_vector = group["proj"](w_vector, self.temp) * self.num_adapters
             print(f"after proj {w_vector}")
 
             j = 0
@@ -487,7 +518,7 @@ class FatAdamW(optim.Optimizer):
 
             if group["w_step"] % self.fat_step == 0:
                 new_chosen_layers = []
-                new_lora_ranks = (self.num_adapters * w_vector * self.default_lora_rank).int()
+                new_lora_ranks = (w_vector * self.default_lora_rank).int()
 
                 j = 0
                 for i, p in enumerate(group["params"]):
